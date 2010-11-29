bbc74c9a7767338c5cb0f4315ab1e23a6d8fda4a
negromanuel.quibar <negromanuel.quibar@5fe3c87b-6f07-5497-2cf6-e1af7f427e48>
2010-11-29 17:35:26 +0000

    Negro: --->

--- branches/Malbec/src/screens/AltaTipoUva.java
+++ branches/Malbec/src/screens/AltaTipoUva.java
@@ -11,6 +11,12 @@
 
 package screens;
 
+import javax.swing.JButton;
+import javax.swing.JTable;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import screens.controllers.ctrlAltaTipoUva;
+
 /**
  *
  * @author Manuel
@@ -18,8 +24,9 @@ package screens;
 public class AltaTipoUva extends javax.swing.JFrame {
 
     /** Creates new form AltaTipoUva */
-    public AltaTipoUva() {
+    public AltaTipoUva(ctrlAltaTipoUva control) {
         initComponents();
+        _control = control;
     }
 
     /** This method is called from within the constructor to
@@ -40,6 +47,16 @@ public class AltaTipoUva extends javax.swing.JFrame {
         _txtDescripcion = new javax.swing.JTextArea();
         jLabel3 = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        _tblParcela = new javax.swing.JTable();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        _tblAsignadas = new javax.swing.JTable();
+        _btnAddOne = new javax.swing.JButton();
+        _btnAddAll = new javax.swing.JButton();
+        _btnRemoveAll = new javax.swing.JButton();
+        _btnRemoveOne = new javax.swing.JButton();
+        _btnOk = new javax.swing.JButton();
+        _btnCancel = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -59,11 +76,10 @@ public class AltaTipoUva extends javax.swing.JFrame {
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 364, Short.MAX_VALUE)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 344, Short.MAX_VALUE)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 380, Short.MAX_VALUE)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabel3)
@@ -79,7 +95,6 @@ public class AltaTipoUva extends javax.swing.JFrame {
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 183, Short.MAX_VALUE)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
@@ -96,26 +111,88 @@ public class AltaTipoUva extends javax.swing.JFrame {
 
         jPanel2.setBorder(javax.swing.BorderFactory.createTitledBorder("Asignación de Parcelas"));
 
+        _tblParcela.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Parcela"
+            }
+        ));
+        jScrollPane2.setViewportView(_tblParcela);
+
+        _tblAsignadas.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Parcela"
+            }
+        ));
+        jScrollPane3.setViewportView(_tblAsignadas);
+
+        _btnAddOne.setText(">");
+
+        _btnAddAll.setText(">|");
+
+        _btnRemoveAll.setText("|<");
+
+        _btnRemoveOne.setText("<");
+
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 364, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 151, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                        .addComponent(_btnAddOne, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(_btnAddAll))
+                    .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                        .addComponent(_btnRemoveOne, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(_btnRemoveAll)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 21, Short.MAX_VALUE)
+                .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 163, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 194, Short.MAX_VALUE)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane3, 0, 0, Short.MAX_VALUE)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 272, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(60, 60, 60)
+                        .addComponent(_btnAddOne)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(_btnAddAll)
+                        .addGap(28, 28, 28)
+                        .addComponent(_btnRemoveOne)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(_btnRemoveAll)))
+                .addContainerGap())
         );
+
+        _btnOk.setText("Ok");
+
+        _btnCancel.setText("Cancel");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+            .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jPanel2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(_btnOk)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(_btnCancel)))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -125,24 +202,26 @@ public class AltaTipoUva extends javax.swing.JFrame {
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(_btnOk)
+                    .addComponent(_btnCancel))
                 .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    /**
-    * @param args the command line arguments
-    */
-    public static void main(String args[]) {
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new AltaTipoUva().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton _btnAddAll;
+    private javax.swing.JButton _btnAddOne;
+    private javax.swing.JButton _btnCancel;
+    private javax.swing.JButton _btnOk;
+    private javax.swing.JButton _btnRemoveAll;
+    private javax.swing.JButton _btnRemoveOne;
+    private javax.swing.JTable _tblAsignadas;
+    private javax.swing.JTable _tblParcela;
     private javax.swing.JTextField _txtCodigo;
     private javax.swing.JTextArea _txtDescripcion;
     private javax.swing.JTextField _txtNombre;
@@ -152,6 +231,54 @@ public class AltaTipoUva extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
     // End of variables declaration//GEN-END:variables
+    private ctrlAltaTipoUva _control;
+    
+    public JButton getBtnAddAll() {
+        return _btnAddAll;
+    }
 
+    public JButton getBtnAddOne() {
+        return _btnAddOne;
+    }
+
+    public JButton getBtnCancel() {
+        return _btnCancel;
+    }
+
+    public JButton getBtnOk() {
+        return _btnOk;
+    }
+
+    public JButton getBtnRemoveAll() {
+        return _btnRemoveAll;
+    }
+
+    public JButton getBtnRemoveOne() {
+        return _btnRemoveOne;
+    }
+
+    public JTable getTblAsignadas() {
+        return _tblAsignadas;
+    }
+
+    public JTable getTblParcela() {
+        return _tblParcela;
+    }
+
+    public JTextField getTxtCodigo() {
+        return _txtCodigo;
+    }
+
+    public JTextArea getTxtDescripcion() {
+        return _txtDescripcion;
+    }
+
+    public JTextField getTxtNombre() {
+        return _txtNombre;
+    }
+
+    
 }
---
+++ branches/Malbec/src/screens/controllers/ctrlAltaTipoUva.java
@@ -0,0 +1,133 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package screens.controllers;
+
+import entidades.Parcela;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import modulo.parcelaTipoUva.ExpAltaTipoUva;
+import screens.AltaTipoUva;
+import screens.models.tablas.TableParcela;
+
+/**
+ *
+ * @author desarrollo
+ */
+public class ctrlAltaTipoUva {
+
+    private AltaTipoUva _pantalla;
+    private ExpAltaTipoUva _gestorAlta;
+    private TableParcela _todas, _asignadas;
+
+    public ctrlAltaTipoUva() {
+        _pantalla = new AltaTipoUva(this);
+        _gestorAlta = new ExpAltaTipoUva();
+        _todas = new TableParcela(_gestorAlta.iniciarAltaTipoUva());
+        _asignadas = new TableParcela(new ArrayList<Parcela>());
+        _pantalla.getTblParcela().setModel(_todas);
+        _pantalla.getTblAsignadas().setModel(_asignadas);
+
+        _pantalla.getBtnOk().addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                pressOkButton();
+            }
+        });
+        _pantalla.getBtnCancel().addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                pressCancelButton();
+            }
+        });
+
+        _pantalla.getBtnAddOne().addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                pressAddOneButton();
+            }
+        });
+        _pantalla.getBtnAddAll().addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                pressAddAllButton();
+            }
+        });
+
+        _pantalla.getBtnRemoveOne().addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                pressRemoveOneButton();
+            }
+        });
+        _pantalla.getBtnRemoveAll().addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                pressRemoveAllButton();
+            }
+        });
+        
+        _pantalla.setVisible(true);
+    }
+
+    void pressOkButton(){
+        try {
+            _gestorAlta.guardarTipoUva(_asignadas.getListRow(), _pantalla.getTxtNombre().getText(), _pantalla.getTxtCodigo().getText(), _pantalla.getTxtDescripcion().getText());
+            JOptionPane.showMessageDialog(_pantalla, "Parcela guardad con exito");
+            _pantalla.getTxtCodigo().setText("");
+            _pantalla.getTxtDescripcion().setText("");
+            _pantalla.getTxtNombre().setText("");
+            _todas = new TableParcela(_gestorAlta.iniciarAltaTipoUva());
+        _asignadas = new TableParcela(new ArrayList<Parcela>());
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(_pantalla, "No se pudo guardar la parcela \n Error: "+ex.getMessage(), "Error al guarda", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    void pressCancelButton(){
+        if(JOptionPane.showConfirmDialog(_pantalla, "Desea cancelar la operacion?", "Confirmar", JOptionPane.QUESTION_MESSAGE)==0)
+            return;
+        _pantalla.dispose();
+    }
+
+    void pressAddOneButton(){
+        int rowSelected = _pantalla.getTblParcela().getSelectedRow();
+        _asignadas.addRow(_todas.getSelectedItem(rowSelected));
+        _todas.getSelectedItem(rowSelected);
+        if(_todas.getListRow().isEmpty()){
+            _pantalla.getBtnAddOne().setEnabled(false);
+            _pantalla.getBtnAddAll().setEnabled(false);
+        }
+    }
+
+    void pressAddAllButton(){
+        _asignadas.addRowAll(_todas.getListRow());
+        _todas.clear();
+        _pantalla.getBtnAddOne().setEnabled(false);
+        _pantalla.getBtnAddAll().setEnabled(false);
+
+    }
+
+    void pressRemoveOneButton(){
+
+        int rowSelected = _pantalla.getTblParcela().getSelectedRow();
+        _todas.addRow(_asignadas.getSelectedItem(rowSelected));
+        _asignadas.getSelectedItem(rowSelected);
+        if(_asignadas.getListRow().isEmpty()){
+            _pantalla.getBtnRemoveOne().setEnabled(false);
+            _pantalla.getBtnRemoveAll().setEnabled(false);
+        }
+    }
+
+    void pressRemoveAllButton(){
+        _todas.addRowAll(_asignadas.getListRow());
+        _asignadas.clear();
+        _pantalla.getBtnRemoveOne().setEnabled(false);
+        _pantalla.getBtnRemoveAll().setEnabled(false);
+
+    }
+}
